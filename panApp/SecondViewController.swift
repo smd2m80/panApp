f435ac8bf181765c08169fdb37e82ccd27cc670c
smd2m80 <2m80cm@gmail.com>
2020-09-08 09:11:26 +0900

    first third Done. WK is error

--- panApp/SecondViewController.swift
+++ panApp/SecondViewController.swift
@@ -8,10 +8,20 @@
 
 import UIKit
 import WebKit
+import CoreMotion
+import simd
 
-class SecondViewController: UIViewController, WKUIDelegate {
+
+class SecondViewController: UIViewController, WKUIDelegate, UIGestureRecognizerDelegate {
 
     var webView: WKWebView!
+    
+    let motionManager = CMMotionManager()
+    var attitude : [CMAttitude] = []
+    var gyro : [CMRotationRate] = []
+    var accZ : [Double] = []
+    var cFLAG = false
+
     
     override func loadView() {
         let webConfiguration = WKWebViewConfiguration()
@@ -21,11 +31,77 @@ class SecondViewController: UIViewController, WKUIDelegate {
     }
     
     override func viewDidLoad() {
+        startSensorUpdates(intervalSeconds: 0.01) // 100Hz
+        
         super.viewDidLoad()
         let myURL = URL(string:"https://ja.wikipedia.org/wiki/%E3%82%AC%E3%83%AA%E3%83%AC%E3%82%AA%E3%83%BB%E3%82%AC%E3%83%AA%E3%83%AC%E3%82%A4")
         let myRequest = URLRequest(url: myURL!)
         webView.load(myRequest)
+        
+        // tapセンサー
+        let tapGesture:UITapGestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(SecondViewController.tapped(_:)))
+        // このときUIGestureRecognizerDelegateに準拠したオブジェクトを設定する
+        tapGesture.delegate = self
+        webView.addGestureRecognizer(tapGesture)
     }
+    
+    func startSensorUpdates(intervalSeconds:Double) {
+        if motionManager.isDeviceMotionAvailable{
+            motionManager.deviceMotionUpdateInterval = intervalSeconds
+            
+            // start sensor updates
+            motionManager.startDeviceMotionUpdates(to: OperationQueue.current!, withHandler: {(motion:CMDeviceMotion?, error:Error?) in
+                self.getMotionData(deviceMotion: motion!)
+                
+            })
+        }
+    }
+    func getMotionData(deviceMotion:CMDeviceMotion) {
+            attitude.append(deviceMotion.attitude)
+            gyro.append(deviceMotion.rotationRate)
+            accZ.append(deviceMotion.userAcceleration.z)
+    }
+    
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//    //           let touch = touches.first!
+//            let accZ200msMin = accZ.suffix(10).min() ?? 0
+//            print(accZ.suffix(20))
+//
+//            if (accZ200msMin < -0.15){
+////                print("touch")
+//                print(self.webView.url ?? "non")
+//        }
+//
+//    }
 
+    @objc func tapped(_ sender: UITapGestureRecognizer){
+        print("tap catch")
+        if sender.state == .ended {
+            
+            print("Ttap")
+            
+            let accZ200msMin = accZ.suffix(10).min() ?? 0
+                        print(accZ.suffix(20))
+
+                        if (accZ200msMin < -0.15){
+                            print("touch")
+                            let activeUrl: URL? = self.webView.url
+                            let url = activeUrl?.absoluteString
+                            print(url!)
+                            alert(title: "コピーしました", message: "")
+            }
+        }
+    }
+    
+    //アラート
+    func alert(title: String, message: String)  {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
 }
 
---
+++ panApp/ThirdViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ThirdViewController.swift
+//  panApp
+//
+//  Created by smd on 2020/08/19.
+//  Copyright © 2020 smd. All rights reserved.
+//
+
+import UIKit
+import CoreMotion
+import simd
+
+class ThirdViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    
+    let motionManager = CMMotionManager()
+    var attitude : [CMAttitude] = []
+    var gyro : [CMRotationRate] = []
+    var accZ : [Double] = []
+    var cFLAG = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        startSensorUpdates(intervalSeconds: 0.01) // 100Hz
+
+    }
+    
+
+    
+    func startSensorUpdates(intervalSeconds:Double) {
+        if motionManager.isDeviceMotionAvailable{
+            motionManager.deviceMotionUpdateInterval = intervalSeconds
+            
+            // start sensor updates
+            motionManager.startDeviceMotionUpdates(to: OperationQueue.current!, withHandler: {(motion:CMDeviceMotion?, error:Error?) in
+                self.getMotionData(deviceMotion: motion!)
+                
+            })
+        }
+    }
+    func getMotionData(deviceMotion:CMDeviceMotion) {
+            attitude.append(deviceMotion.attitude)
+            gyro.append(deviceMotion.rotationRate)
+            accZ.append(deviceMotion.userAcceleration.z)
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        DispatchQueue.main.async {
+            if self.cFLAG{
+                textView.selectAll(self)
+            }else{
+                // 選択を解除したい
+                textView.selectedRange = NSRange()
+            }
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//           let touch = touches.first!
+        let accZ200msMin = accZ.suffix(10).min() ?? 0
+        print(accZ.suffix(20))
+
+        if (accZ200msMin < -0.15){
+            cFLAG = true
+        }else{
+            cFLAG = false
+        }
+        textViewDidBeginEditing(textView)
+        textView.resignFirstResponder()
+        
+       }
+
+
+}
